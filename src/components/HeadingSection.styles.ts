51cff086eea5d5bb33117106792c81e2b6abae8b
Karol <karol99kos@gmail.com>
2022-07-01 00:51:30 +0200

    Add title color animation

--- src/components/HeadingSection.styles.ts
+++ src/components/HeadingSection.styles.ts
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import { flexDefault } from 'styles/shared';
 
+import { keyframes } from 'styled-components';
+
 export const Root = styled.header`
   ${flexDefault};
   justify-content: flex-start;
   line-height: 0.85;
-  color: #FC6E6E;
+  color: #fff;
   font-size: 2.4rem;
   /* border: 2px solid green; */
 
@@ -14,6 +16,18 @@ export const Root = styled.header`
   }
 `;
 
-export const Surname = styled.span`
-  color: #fff
+export const colorAnimation = keyframes`
+  0%    {color: #FC6E6E}
+  32%   {color: #FC6E6E}
+  33%   {color: #e4cb58}
+  65%   {color: #e4cb58}
+  66%   {color: #8fc866}
+  99%   {color: #8fc866}
+  100%  {color: #FC6E6E}
+`
+
+export const Name = styled.span`
+  color: #fff;
+  animation: ${colorAnimation} 8s linear infinite;
+  transition: 1s;
 `;
--- src/components/HeadingSection.tsx
+++ src/components/HeadingSection.tsx
@@ -1,11 +1,11 @@
-import { Root, Surname } from "./HeadingSection.styles"
+import { Root, Name } from "./HeadingSection.styles"
 
 const HeadingSection = () => {
   return (
     <Root>
         <h1>
-          KAROL <br/>
-          <Surname>KOSEK</Surname>
+          <Name>KAROL</Name><br/>
+          KOSEK
         </h1>
     </Root>
   )
